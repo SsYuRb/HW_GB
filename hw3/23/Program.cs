6d6da5c75ea0cb538d0b584de6ce182b83029bc3
SsYuRb <113142100+SsYuRb@users.noreply.github.com>
2022-10-05 20:28:17 +0300

    ex23

---
+++ hw3/23/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите число: ");
+int number = Int32.Parse(Console.ReadLine());
+void Cubes (int numb1)
+{
+    for (int i = 1; i < numb1+1; i++)
+    {
+        int cube = i*i*i;
+        Console.WriteLine(cube);
+    }
+}
+Console.WriteLine("Ваши кубы: ");
+Cubes(number);
