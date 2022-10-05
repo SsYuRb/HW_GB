6395e87f3ec80ce455df2086807e2c4333187316
SsYuRb <113142100+SsYuRb@users.noreply.github.com>
2022-10-05 20:26:57 +0300

    ex21

---
+++ hw3/19/Program.cs
@@ -0,0 +1,26 @@
+﻿void Pallindrom(string number)
+{
+    int lenght_number = number.Length;
+    string digit = "";
+    int numbers = Int32.Parse(number);
+    int numbers_reserv = Int32.Parse(number);
+    while (lenght_number > 0)
+    {
+        int digits = numbers % 10;
+        digit = digit + digits.ToString();
+        numbers = numbers/10;
+        lenght_number = lenght_number - 1;
+    }
+    int digit1 = Int32.Parse(digit);
+    if (digit1 == numbers_reserv)
+    {
+        Console.WriteLine("Ваше число палиндром.");
+    }
+    else 
+    {
+        Console.WriteLine("Нет, Ваше число не палиндром");
+    }
+}
+Console.WriteLine("Введите число: ");
+string input = Console.ReadLine();
+Pallindrom(input);
---
+++ hw3/21/Program.cs
@@ -0,0 +1,19 @@
+﻿void Distance (int a1, int b1, int c1, int a2, int b2, int c2)
+{
+    int distance = (a1-a2)*(a1-a2)+(b1-b2)*(b1-b2)+(c1-c2)*(c1-c2);
+    double sqrt = Math.Sqrt(distance);
+    Console.WriteLine(sqrt);
+}
+Console.WriteLine ("Введите х1: ");
+int x1 = Int32.Parse(Console.ReadLine());
+Console.WriteLine ("Введите у1: ");
+int y1 =  Int32.Parse(Console.ReadLine());
+Console.WriteLine ("Введите z1: ");
+int z1 =  Int32.Parse(Console.ReadLine());
+Console.WriteLine ("Введите х2: ");
+int x2 =  Int32.Parse(Console.ReadLine());
+Console.WriteLine ("Введите у2: ");
+int y2 =  Int32.Parse(Console.ReadLine());
+Console.WriteLine ("Введите z2: ");
+int z2 =  Int32.Parse(Console.ReadLine());
+Distance(x1,y1,z1,x2,y2,z2);
