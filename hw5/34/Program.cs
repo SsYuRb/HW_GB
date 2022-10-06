eba737ccc1eecf4ff1857dc54e031538673bc593
SsYuRb <113142100+SsYuRb@users.noreply.github.com>
2022-10-06 11:54:56 +0300

    End ex 34 hw5

---
+++ hw5/34/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine("Введите количество чисел: ");
+int digits = Int32.Parse(Console.ReadLine());
+int[] arrayDigits = new int[digits];
+for (int i = 0; i<digits; i++)
+{
+    if (i == 0)
+    {
+        Console.Write("Введите число: ");
+    }
+    arrayDigits[i] = Int32.Parse(Console.ReadLine());
+    if (i != digits-1)
+    {
+        Console.Write("Введите следующее число: ");
+    }
+}
+int count = 0;
+for (int i = 0; i<digits; i++)
+{
+    if (arrayDigits[i]%2==0) {count += 1; }
+}
+Console.Write("Колличество четных чисел: ");
+Console.WriteLine(count);
