aa8703ad52f05e00018f5b09df1ed231109e61d3
SsYuRb <113142100+SsYuRb@users.noreply.github.com>
2022-10-06 12:34:51 +0300

    End hw5

--- hw5/36/Program.cs
+++ hw5/36/Program.cs
@@ -1,20 +1,20 @@
 ﻿Console.WriteLine("Введите количество чисел: ");
 int digits = Int32.Parse(Console.ReadLine());
 int[] arrayDigits = new int[digits];
-for (int i = 0; i<digits; i++)
+for (int i = 0; i < digits; i++)
 {
     if (i == 0)
     {
         Console.Write("Введите число: ");
     }
     arrayDigits[i] = Int32.Parse(Console.ReadLine());
-    if (i != digits-1)
+    if (i != digits - 1)
     {
         Console.Write("Введите следующее число: ");
     }
 }
 int summ = 0;
-for(int i=1; i<digits; i += 2)
+for (int i = 1; i < digits; i += 2)
 {
     summ = summ + arrayDigits[i];
 }
---
+++ hw5/38/Program.cs
@@ -0,0 +1,32 @@
+﻿Console.WriteLine("Введите количество чисел: ");
+int digits = Int32.Parse(Console.ReadLine());
+double[] arrayDigits = new double[digits];
+
+for (int i = 0; i < digits; i++)
+{
+    if (i == 0)
+    {
+        Console.Write("Введите число: ");
+    }
+    arrayDigits[i] = double.Parse(Console.ReadLine());
+    if (i != digits - 1)
+    {
+        Console.Write("Введите следующее число: ");
+    }
+}
+
+double maxNumber = arrayDigits[0];
+double minNumber = arrayDigits[0];
+
+for (int i = 0; i < digits; i++)
+{
+    if (arrayDigits[i] > maxNumber) { maxNumber = arrayDigits[i]; }
+    else if (arrayDigits[i] < minNumber) { minNumber = arrayDigits[i]; }
+}
+Console.Write("Максимальное введенное число: ");
+Console.WriteLine(maxNumber);
+Console.Write("Миниамльное введенное число: ");
+Console.WriteLine(minNumber);
+Console.Write("Разница между максимальным и миниамльным введенным числом равна: ");
+double dif = maxNumber - minNumber;
+Console.WriteLine(dif);
