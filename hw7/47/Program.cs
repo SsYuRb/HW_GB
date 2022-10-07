0bd47eeaa54d62c2dd919a3fdf2ff1412fa98545
SsYuRb <113142100+SsYuRb@users.noreply.github.com>
2022-10-07 23:17:05 +0300

    End ex47 hw7

---
+++ hw7/47/Program.cs
@@ -0,0 +1,39 @@
+﻿double[,] numbers = new double[3, 4];
+
+void PrintArray (double [,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} | ");
+        }
+    Console.WriteLine();
+    }
+}
+
+void FillRandomNumbers (double [,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i,j] = new Random().NextDouble()*10;
+        }
+    }
+}
+
+void RoundArrayNumbers (double [,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i,j] = Math.Round(array[i,j], 2);
+        }
+    }
+}
+
+FillRandomNumbers(numbers);
+RoundArrayNumbers(numbers);
+PrintArray(numbers);
