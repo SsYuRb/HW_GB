b1aa2951ab0639dc8f6459f0a54ad6e63fc1178f
SsYuRb <113142100+SsYuRb@users.noreply.github.com>
2022-10-16 01:47:50 +0300

    Not end ex62 hw8

---
+++ hw8/62/Program.cs
@@ -0,0 +1,41 @@
+﻿int [,] arrayNum = new int [4,4];
+int count = 1;
+
+void FillArray (int[,] array)
+{
+    for (int j = 0; j < array.GetLength(0); j++)
+    {
+        array [0, j] = count;
+        count += 1;
+    }
+    for (int i = 1; i < array.GetLength(0); i++)
+    {
+        array[i, array.GetLength(1)-1] = count;
+        count += 1;
+    }
+    for (int j = array.GetLength(1)-2; j != -1; j--)
+    {
+        array [array.GetLength(0)-1, j] = count;
+        count += 1;
+    }
+    for (int i = array.GetLength(0)-2; i != 0; i--)
+    {
+        array[i, 0] = count;
+        count += 1;
+    }
+}
+FillArray(arrayNum);
+
+void PrintArray (int[,] matrix)
+{ 
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]} ");
+        }
+    Console.WriteLine();
+    }
+}
+
+PrintArray(arrayNum);
